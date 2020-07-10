e3308b9c3d5573c008c71c10f1d9c4ee3c5ad311
Mauricio Viana <mau_viana@live.com>
2020-07-10 10:37:07 -0300

    Adicionado rota para documentação.

--- src/app.js
+++ src/app.js
@@ -25,15 +25,15 @@ import { db } from './models/index.js';
 const app = express();
 
 //define o dominio de origem para consumo do servico
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(gradeRouter);
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(
   cors({
     origin: process.env.APP_URL,
   })
 );
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(gradeRouter);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(process.env.PORT || 8081, () => {
   logger.info(`Servidor em execucao na porta ${process.env.PORT}`);
